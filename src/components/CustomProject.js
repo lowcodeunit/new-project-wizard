21bfede74445ee69ab95978160e6e81952933dd2
mcgear <michael.gearhardt@gmail.com>
2022-01-26 09:50:32 -0700

    width fix on text

--- src/components/CustomProject.js
+++ src/components/CustomProject.js
@@ -329,7 +329,7 @@ class CustomProject extends LCUComponent {
               onChange={this.handleOutputChange}
               defaultValue={this.state.buildOutput}
             />
-            <p>
+            <p sx={{ maxWidth: '300px', width: '40%' }}>
               The output is the directory that the built assets for your project
               are in. For React this is './build', for Angular and Vue is
               './dist', and for Svelte is './public'.
