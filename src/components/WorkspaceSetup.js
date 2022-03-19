adc6c73d209dbddad38e199a0730915e7faa4efd
mcgear <michael.gearhardt@gmail.com>
2022-03-19 07:59:42 -0600

    Adjusted to have different logic for MFE buttons

--- src/components/WorkspaceSetup.js
+++ src/components/WorkspaceSetup.js
@@ -41,6 +41,62 @@ function WorkspaceSetup(props) {
     });
     navigate('/deploy');
   }
+
+  let buttonBox;
+
+  if (recipe.RecipeType == 'MFE') {
+    buttonBox = (
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Button
+          variant="contained"
+          className="recipeButtons"
+          sx={{ mr: 1 }}
+          value={item}
+          onClick={() => handleForkClick(item)}
+        >
+          Launch Micro Frontend
+        </Button>
+      </Box>
+    );
+  } else {
+    buttonBox = (
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Button
+          variant="contained"
+          className="recipeButtons"
+          sx={{ mr: 1 }}
+          value={item}
+          onClick={() => handleForkClick(item)}
+        >
+          Fork
+        </Button>
+        <Button
+          variant="contained"
+          className="recipeButtons"
+          onClick={() => handleOpenSource(item)}
+        >
+          Launch
+        </Button>
+      </Box>
+    );
+  }
+
   let recipeSection = (
     <Box
       sx={{
@@ -50,94 +106,129 @@ function WorkspaceSetup(props) {
         mx: 4,
       }}
     >
-      <Box
-        sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}
-      >
+      <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
         {props.recipeList.map((item) => (
           <Box>
             <Grid container spacing={1} sx={{ mt: 2 }}>
               <Grid item xs={2} md={2}>
-
-                <img className='recipeImage' src={item.PreviewImage} alt={item.Lookup} />
+                <img
+                  className="recipeImage"
+                  src={item.PreviewImage}
+                  alt={item.Lookup}
+                />
               </Grid>
               <Grid item xs={4} md={4}>
                 <Box>
                   <Typography
-                    sx={{ fontWeight: ['600', '600', '900'], fontSize: ['20px']}}
-                    align='left'
+                    sx={{
+                      fontWeight: ['600', '600', '900'],
+                      fontSize: ['20px'],
+                    }}
+                    align="left"
                     noWrap={true}
                   >
                     {capitalize(item.Lookup)}
                   </Typography>
                   <Typography
-                    sx={{ fontWeight: '400', fontSize: ['15px'], display: ['none', 'none', 'block'] }}
+                    sx={{
+                      fontWeight: '400',
+                      fontSize: ['15px'],
+                      display: ['none', 'none', 'block'],
+                    }}
                     noWrap={false}
-                    align='left'
+                    align="left"
                   >
-                    {`${item.Description.substring(0, 80)}...`}<Link color="primary" to={`/recipe/${item.Lookup}`}>More Info</Link>
+                    {`${item.Description.substring(0, 80)}...`}
+                    <Link color="primary" to={`/recipe/${item.Lookup}`}>
+                      More Info
+                    </Link>
                   </Typography>
                 </Box>
               </Grid>
               <Grid item xs={6} md={6}>
-                <Box sx={{ width: '100%', alignItems: "center", justifyContent: 'flex-end', pt: [0, 1, 4] }}>
-                  <Box sx={{ display: 'flex', flexDirection: 'row', width: '100%', alignItems: "center", justifyContent: 'center' }}>
-                    <Button
-                      variant="contained"
-                      className='recipeButtons'
-                      sx={{ mr: 1 }}
-                      value={item}
-                      onClick={() => handleForkClick(item)}
-                    >
-                      Fork
-                    </Button>
-                    <Button
-                      variant="contained"
-                      className='recipeButtons'
-                      onClick={() => handleOpenSource(item)}
-                    >
-                      Launch
-                    </Button>
-                  </Box>
-                  <Link className='recipeLink' color="primary" to={`/recipe/${item.Lookup}`}>More Info</Link>
-                </Box>
+                <Box
+                  sx={{
+                    width: '100%',
+                    alignItems: 'center',
+                    justifyContent: 'flex-end',
+                    pt: [0, 1, 4],
+                  }}
+                >
+                  {buttonBox}
 
+                  <Link
+                    className="recipeLink"
+                    color="primary"
+                    to={`/recipe/${item.Lookup}`}
+                  >
+                    More Info
+                  </Link>
+                </Box>
               </Grid>
             </Grid>
             <hr />
           </Box>
         ))}
       </Box>
-    </Box >
+    </Box>
   );
 
   return (
-    <Box sx={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexDirection: 'column'
-    }}>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
       <Helmet>
         <title>Fathym - Select your project</title>
       </Helmet>
-      <Paper sx={{ width: ['90%', '80%', '60%'], display: 'flex', flexDirection: 'column', my: 2, py: 2 }} elevation={6}>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', pl: [0, 0, 4], width: '100%', flexDirection: { xs: 'column', sm: 'column', md: 'row' } }}>
+      <Paper
+        sx={{
+          width: ['90%', '80%', '60%'],
+          display: 'flex',
+          flexDirection: 'column',
+          my: 2,
+          py: 2,
+        }}
+        elevation={6}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            pl: [0, 0, 4],
+            width: '100%',
+            flexDirection: { xs: 'column', sm: 'column', md: 'row' },
+          }}
+        >
           <h2>Get started with a template</h2>
-          <Box sx={{ display: 'flex', justifyContent: { xs: 'center', sm: 'center', md: 'flex-end' } }}>
-            <Button sx={{ mr: [0, 0, 5], height: "70%" }} variant="contained" onClick={handleCustomClick}>Create custom project</Button>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: { xs: 'center', sm: 'center', md: 'flex-end' },
+            }}
+          >
+            <Button
+              sx={{ mr: [0, 0, 5], height: '70%' }}
+              variant="contained"
+              onClick={handleCustomClick}
+            >
+              Create custom project
+            </Button>
           </Box>
         </Box>
         <Box sx={{ display: 'flex', justifyContent: 'flex-start', pl: 4 }}>
           <p>
-            Launch from our open source repo or fork to yours to launch.
-            Note: to customize your code you must fork to your own repo
+            Launch from our open source repo or fork to yours to launch. Note:
+            to customize your code you must fork to your own repo
           </p>
         </Box>
-        <Box>
-          {recipeSection}
-        </Box>
-      </Paper >
-    </Box >
+        <Box>{recipeSection}</Box>
+      </Paper>
+    </Box>
   );
 }
 
